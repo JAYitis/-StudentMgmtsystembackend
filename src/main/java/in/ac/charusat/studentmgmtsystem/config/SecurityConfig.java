717c67f2544a2f638df45e3e08f30a7005b7e555
JAYitis <20IT097@charusat.edu.in>
2022-09-12 22:40:17 +0530

    Added security with custom users jdbc authentication using H2 database

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package in.ac.charusat.studentmgmtsystem.config;
 
 import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -21,6 +25,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("pass")
                 .roles("USER");
+        //        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("pass")
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("user")
+//                .password("pass")
+//                .roles("USER");
     }
 
     @Bean
